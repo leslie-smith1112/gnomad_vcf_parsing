1e23e4c510e38555895771b44ccb3ceed2853217
Leslie Smith <leslie.smith1@login6.ufhpc>
2022-03-03 20:34:08 -0500

    Code for ancestry probability calculations.

---
+++ build/CMakeFiles/read_freq.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/read_freq.dir/main.cpp.o"
+  "CMakeFiles/read_freq.dir/main.cpp.o.d"
+  "read_freq"
+  "read_freq.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/read_freq.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/read_freq.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for read_freq.
+# This may be replaced when dependencies are built.
